a7ec9afa8cea3617996712ca9b97eb6bee9de2e4
Karan Jeswani <34395853+codewithkaranjeswani@users.noreply.github.com>
2018-04-27 07:01:37 +0530

    CC1.1_MatrixOperations
    Adding Multiplication operation for matrices

---
+++ CC1.1_MatrixOperations.py
@@ -0,0 +1,111 @@
+def inverse(here):
+    inverse = []
+    det = determinant(here)
+    for f in range(len(here)):
+        temp = []
+        for g in range(len(here[f])):
+            temp.append(0)
+        inverse.append(temp)
+    
+    for i in range(len(here)):
+        for j in range(len(here[i])):
+            if (i + j) % 2 == 0:
+                inverse[j][i] = 1 / det * determinant(submatrix(here, i, j))
+            else:
+                inverse[j][i] = -1 / det * determinant(submatrix(here, i, j))
+    return inverse
+
+def submatrix(hi, i, j):
+    eleven = []
+    for f in range(len(hi)):
+        temp = []
+        for g in range(len(hi[f])):
+            temp.append(hi[f][g])
+        eleven.append(temp)
+
+    del eleven[i]
+    for h in range(len(eleven)):
+        del eleven[h][j]
+
+    return eleven
+
+def determinant(thatone):
+# evaluating determinant using first row ie. row number 0
+    if len(thatone) == 1:
+        return thatone[0][0]
+    else:
+        ans = 0
+        for i in range(len(thatone[0])):
+            if i % 2 == 0:
+                ans += thatone[0][i] * determinant(submatrix(thatone, 0, i))
+            else:
+                ans -= thatone[0][i] * determinant(submatrix(thatone, 0, i))
+    return ans
+
+def transpose(samehere):
+# len(samehere[0]) give the number of columns in the matrix
+# len(samehere) gives the number of rows in the matrix
+    thisone = []
+    for j in range(len(samehere[0])):
+        temp = []
+        for i in range(len(samehere)):
+            temp.append(samehere[i][j])
+        thisone.append(temp)
+    return thisone
+
+def display(taking):
+    for i in range(len(taking)):
+        for j in range(len(taking[i])):
+            print(repr(taking[i][j]).rjust(5), end=" ")
+        print()
+
+def multiply(a, b):
+# a (mxn) and b (nxp)
+    ans = []
+    if (len(a[0]) == len(b)):
+        n = len(b)
+        m = len(a)
+        p = len(b[0])
+        for i in range(m):
+            temp = []
+            for j in range(p):
+                product = 0
+                for k in range(n):
+                    product += a[i][k] * b[k][j]
+                temp.append((product))
+            ans.append(temp)
+        return ans
+    else:
+        print("check order")
+        return ans
+    
+def main():
+    given = [[97, -77],
+             [89.6, -67.2],
+             [91.5, -69],
+             [17.2, -2.4],
+             [100.6, -80.2],
+             [92.9, -69.8],
+             [88, -67]]
+    
+    another = [[8, 2],
+               [9, 1]]
+#    detval = determinant(given)
+#    inversed = inverse(given)
+
+#    print('\nGiven matrix:')
+    print('First Matrix: ')
+    display(given)
+    print()
+#    print('determinant = ' + str(detval))
+    print('Second Matrix: ')
+    display(another)
+    print()
+#    print('Inverse:')
+    print('Product ')
+    display(multiply(given, another))
+    print()
+#    display(inversed)
+    
+if __name__ == '__main__':
+        main()
